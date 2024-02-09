027a5bdd58bebc2c51465b16aece83d49bb9da85
majtanmarek <marek.majtan@all4labels.com>
2024-02-09 11:47:51 +0100

    kk

--- src/GeneralCollection.php
+++ src/GeneralCollection.php
@@ -9,14 +9,4 @@ class GeneralCollection extends ResourceCollection
     // For keeping keys in arrays. When false then keys will be lost if indexes are numeric value.
     protected $preserveKeys = true;
     
-    /**
-     * Transform the resource collection into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function toArray($request)
-    {
-        return parent::toArray($request);
-    }
 }
