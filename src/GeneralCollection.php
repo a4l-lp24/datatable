1c41d5d537c48662fac0e0571dc1539044f6d759
majtanmarek <marek.majtan@all4labels.com>
2024-02-12 10:10:58 +0100

    fix: GeneralCollection adjusted to support collection resource

--- src/GeneralCollection.php
+++ src/GeneralCollection.php
@@ -8,5 +8,19 @@ class GeneralCollection extends ResourceCollection
 {
     // For keeping keys in arrays. When false then keys will be lost if indexes are numeric value.
     protected $preserveKeys = true;
-    
+
+    /**
+     * Transform the resource into a JSON array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'data' => $this->collection,
+            'links' => [],
+        ];
+    }
+
 }
